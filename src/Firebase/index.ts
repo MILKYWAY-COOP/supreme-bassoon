3877ebc2499c8bac9fd1872e62027c5e0816c61f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 03:39:07 +0300

    Starter files

---
+++ src/Firebase/index.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkP-xy_PdO8gclPNmtYU5raGXcDeZOdzc',
+  authDomain: 'elections-f40cd.firebaseapp.com',
+  projectId: 'elections-f40cd',
+  storageBucket: 'elections-f40cd.appspot.com',
+  messagingSenderId: '864336667413',
+  appId: '1:864336667413:web:77c3648c7c1ca5b347ff1c'
+};
+
+const app = initializeApp(firebaseConfig);
