e8db9c8682feddf1ec96614eae7ea1ee9828da93
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 15:50:27 +0300

    Kenya

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Choice } from './Choice/Choice';
 export { default as Rbar } from './RBar/Rbar';
+export { default as Kenya } from './Kenya/Kenya';
