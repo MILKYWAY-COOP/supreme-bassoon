253bc7fc799dea50d6057d4a4304b07e4e874c11
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 09:56:47 +0300

    Data & Choice

--- src/Elements/index.ts
+++ src/Elements/index.ts
@@ -1,9 +1,13 @@
-import  Kenya  from './Files/kenya.svg';
+import Kenya from './Files/kenya.svg';
 import Citizen from './Files/logo.webp';
 import Choice from './Files/choice.png';
+import Ruto from './Files/ruto.webp';
+import Rao from './Files/rao.webp';
 
 export const Elements = {
   Kenya,
   Citizen,
-  Choice
+  Choice,
+  Ruto,
+  Rao
 };
