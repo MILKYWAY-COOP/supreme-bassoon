20e41fc06d3eb898c0d2859aaa9be7f1bf0818bc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 13:56:25 +0300

    Bar Card

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,11 @@
-import { Choice } from '../Components';
+import { Choice, Rbar } from '../Components';
 import Styles from './Home.module.scss';
 
 export const Home = () => {
   return (
     <div className={Styles.home}>
       <Choice />
+      <Rbar />
     </div>
   );
 };
