1116dee33a1774719d7678130a059f85d2e51c59
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 07:09:46 +0300

    NavBar

---
+++ src/Elements/index.ts
@@ -0,0 +1,9 @@
+import  Kenya  from './Files/kenya.svg';
+import Citizen from './Files/logo.webp';
+import Choice from './Files/choice.png';
+
+export const Elements = {
+  Kenya,
+  Citizen,
+  Choice
+};
