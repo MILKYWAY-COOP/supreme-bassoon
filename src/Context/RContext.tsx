bbe784dc6adcd300b9ec46ee78d10ebd201925aa
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 15:50:46 +0300

    Kenya

--- src/Context/RContext.tsx
+++ src/Context/RContext.tsx
@@ -3,7 +3,8 @@ import results2022 from '../Data/2022';
 
 // Create a interface for getCountyTotal, overallTotals
 interface IResultsContext {
-  getCountyTotal: (county: string) => {
+  results: {};
+  getCountyTotal: (county: number) => {
     RaoTotal: number;
     RutoTotal: number;
     WaihigaTotal: number;
@@ -14,6 +15,10 @@ interface IResultsContext {
   RutoWilliamTotal: number;
   WaihigaDavidTotal: number;
   WajackoyahGeorgeTotal: number;
+  getTopCandidate: (county: number) => {
+    name: string;
+    votes: number;
+  };
 }
 
 const ResultsContext = createContext<IResultsContext | null>(null);
@@ -42,22 +47,21 @@ export const ResultsProvider = ({ children }: Props) => {
     RutoWilliamTotal += results[i].RutoWilliam;
     WaihigaDavidTotal += results[i].WaihigaDavid;
     WajackoyahGeorgeTotal += results[i].WajackoyahGeorge;
-    }
-    
+  }
+
   let totals =
     RailaOdingaTotal +
     RutoWilliamTotal +
     WaihigaDavidTotal +
     WajackoyahGeorgeTotal;
 
-  function getCountyTotal(county: string) {
-    county = county.toUpperCase();
+  function getCountyTotal(county: number) {
     let RaoTotal = 0;
     let RutoTotal = 0;
     let WaihigaTotal = 0;
     let WajackoyahTotal = 0;
     for (let i = 0; i < results.length; i++) {
-      if (results[i].County === county) {
+      if (results[i].Id === county) {
         RaoTotal += results[i].RailaOdinga;
         RutoTotal += results[i].RutoWilliam;
         WaihigaTotal += results[i].WaihigaDavid;
@@ -65,6 +69,24 @@ export const ResultsProvider = ({ children }: Props) => {
       }
     }
     return { RaoTotal, RutoTotal, WaihigaTotal, WajackoyahTotal };
+  }
+
+  // get the top candidate in a county
+  function getTopCandidate(county: number) {
+    const { RaoTotal, RutoTotal, WaihigaTotal, WajackoyahTotal } =
+      getCountyTotal(county);
+    const candidates = [
+      { name: 'Raila Odinga', votes: RaoTotal },
+      { name: 'Ruto William', votes: RutoTotal },
+      { name: 'Waihiga David', votes: WaihigaTotal },
+      { name: 'Wajackoyah George', votes: WajackoyahTotal }
+    ];
+
+    // sort the candidates by votes
+    const sortedCandidates = candidates.sort((a, b) => b.votes - a.votes);
+    // get the candidate with the highest votes
+    const topCandidate = sortedCandidates[0];
+    return topCandidate;
   }
 
   return (
@@ -75,7 +97,9 @@ export const ResultsProvider = ({ children }: Props) => {
         WaihigaDavidTotal,
         WajackoyahGeorgeTotal,
         totals,
-        getCountyTotal
+        getCountyTotal,
+        results,
+        getTopCandidate
       }}
     >
       {children}
