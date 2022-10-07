6e9d62bbb85647743b4a2bff171e4bebada62721
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 09:56:07 +0300

    Data & Choice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from './Components';
+import { Home } from './Home/Home';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Home />
     </div>
   );
 }
