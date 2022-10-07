18b62aab924c9abf060563c75a2435e94fe1c048
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 14:05:19 +0300

    'ResultsContext' is already defined

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,6 +2,13 @@ import { Choice, Rbar } from '../Components';
 import Styles from './Home.module.scss';
 
 export const Home = () => {
+  //get the current date, and time in hours and minutes
+  const date = new Date();
+  const time = date.getHours();
+  const minutes = date.getMinutes();
+
+  console.log(time, minutes);
+
   return (
     <div className={Styles.home}>
       <Choice />
