af92a090402b09d393be0af020c9a8804ae62ed7
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 09:56:21 +0300

    Data & Choice

---
+++ src/Components/Choice/Choice.tsx
@@ -0,0 +1,20 @@
+import { Elements } from '../../Elements';
+import Styles from './Choice.module.scss';
+
+const Choice = () => {
+  return (
+    <div className={Styles.main}>
+      <div className={Styles.image}>
+        <img src={Elements.Choice} />
+      </div>
+      <div className={Styles.years}>
+        <span>2022</span>
+        <span>2022 - RERUN</span>
+        <span>2017</span>
+        <span>2013</span>
+      </div>
+    </div>
+  );
+};
+
+export default Choice;
