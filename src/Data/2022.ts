ed6783d6d4f950cb9ff88acb362b310a9a044801
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-07 09:56:44 +0300

    Data & Choice

---
+++ src/Data/2022.ts
@@ -0,0 +1,340 @@
+const results2022 = [
+  {
+    County: 'MOMBASA',
+    RailaOdinga: 161015,
+    RutoWilliam: 113700,
+    WajackoyahGeorge: 2104,
+    WaihigaDavid: 482
+  },
+  {
+    County: 'KWALE',
+    RailaOdinga: 125541,
+    RutoWilliam: 51918,
+    WajackoyahGeorge: 1137,
+    WaihigaDavid: 413
+  },
+  {
+    County: 'KILIFI',
+    RailaOdinga: 204536,
+    RutoWilliam: 77331,
+    WajackoyahGeorge: 2552,
+    WaihigaDavid: 1067
+  },
+  {
+    County: 'TANA RIVER',
+    RailaOdinga: 51390,
+    RutoWilliam: 41505,
+    WajackoyahGeorge: 412,
+    WaihigaDavid: 198
+  },
+  {
+    County: 'LAMU',
+    RailaOdinga: 26160,
+    RutoWilliam: 22876,
+    WajackoyahGeorge: 848,
+    WaihigaDavid: 186
+  },
+  {
+    County: 'TAITA TAVETA',
+    RailaOdinga: 81271,
+    RutoWilliam: 22876,
+    WajackoyahGeorge: 826,
+    WaihigaDavid: 249
+  },
+  {
+    County: 'GARISSA',
+    RailaOdinga: 81376,
+    RutoWilliam: 28111,
+    WajackoyahGeorge: 396,
+    WaihigaDavid: 77
+  },
+  {
+    County: 'WAJIR',
+    RailaOdinga: 83486,
+    RutoWilliam: 49062,
+    WajackoyahGeorge: 626,
+    WaihigaDavid: 217
+  },
+  {
+    County: 'MANDERA',
+    RailaOdinga: 106279,
+    RutoWilliam: 28351,
+    WajackoyahGeorge: 676,
+    WaihigaDavid: 259
+  },
+  {
+    County: 'MARSABIT',
+    RutoWilliam: 58782,
+    RailaOdinga: 55675,
+    WajackoyahGeorge: 303,
+    WaihigaDavid: 144
+  },
+  {
+    County: 'ISIOLO',
+    RutoWilliam: 32302,
+    RailaOdinga: 26449,
+    WajackoyahGeorge: 175,
+    WaihigaDavid: 52
+  },
+  {
+    County: 'MERU',
+    RutoWilliam: 398946,
+    RailaOdinga: 103679,
+    WajackoyahGeorge: 2798,
+    WaihigaDavid: 1053
+  },
+  {
+    County: 'THARAKA - NITHI',
+    RutoWilliam: 145081,
+    RailaOdinga: 15062,
+    WajackoyahGeorge: 974,
+    WaihigaDavid: 461
+  },
+  {
+    County: 'EMBU',
+    RutoWilliam: 187981,
+    RailaOdinga: 31209,
+    WajackoyahGeorge: 1335,
+    WaihigaDavid: 523
+  },
+  {
+    County: 'KITUI',
+    RailaOdinga: 235402,
+    RutoWilliam: 89419,
+    WajackoyahGeorge: 2840,
+    WaihigaDavid: 1412
+  },
+  {
+    County: 'MACHAKOS',
+    RailaOdinga: 304809,
+    RutoWilliam: 101456,
+    WajackoyahGeorge: 2903,
+    WaihigaDavid: 1070
+  },
+  {
+    County: 'MAKUENI',
+    RailaOdinga: 229213,
+    RutoWilliam: 59195,
+    WajackoyahGeorge: 1836,
+    WaihigaDavid: 575
+  },
+  {
+    County: 'NYANDARUA',
+    RutoWilliam: 189519,
+    RailaOdinga: 49228,
+    WajackoyahGeorge: 1048,
+    WaihigaDavid: 821
+  },
+  {
+    County: 'NYERI',
+    RutoWilliam: 272507,
+    RailaOdinga: 52052,
+    WajackoyahGeorge: 1339,
+    WaihigaDavid: 982
+  },
+  {
+    County: 'KIRINYAGA',
+    RutoWilliam: 220984,
+    RailaOdinga: 37909,
+    WajackoyahGeorge: 1367,
+    WaihigaDavid: 640
+  },
+  {
+    County: "MURANG'A",
+    RutoWilliam: 220984,
+    RailaOdinga: 37909,
+    WajackoyahGeorge: 1367,
+    WaihigaDavid: 640
+  },
+  {
+    County: 'KIAMBU',
+    RutoWilliam: 606429,
+    RailaOdinga: 210580,
+    WajackoyahGeorge: 4377,
+    WaihigaDavid: 3805
+  },
+  {
+    County: 'TURKANA',
+    RailaOdinga: 96117,
+    RutoWilliam: 46696,
+    WajackoyahGeorge: 445,
+    WaihigaDavid: 274
+  },
+  {
+    County: 'WEST POKOT',
+    RutoWilliam: 109944,
+    RailaOdinga: 63092,
+    WajackoyahGeorge: 375,
+    WaihigaDavid: 194
+  },
+  {
+    County: 'SAMBURU',
+    RailaOdinga: 41737,
+    RutoWilliam: 28329,
+    WajackoyahGeorge: 220,
+    WaihigaDavid: 62
+  },
+  {
+    County: 'TRANS NZOIA',
+    RailaOdinga: 132440,
+    RutoWilliam: 116776,
+    WajackoyahGeorge: 883,
+    WaihigaDavid: 414
+  },
+  {
+    County: 'UASIN GISHU',
+    RutoWilliam: 272868,
+    RailaOdinga: 76009,
+    WajackoyahGeorge: 594,
+    WaihigaDavid: 392
+  },
+  {
+    County: 'ELGEYO MARAKWET',
+    RutoWilliam: 160033,
+    RailaOdinga: 4893,
+    WajackoyahGeorge: 176,
+    WaihigaDavid: 122
+  },
+  {
+    County: 'NANDI',
+    RutoWilliam: 280813,
+    RailaOdinga: 26034,
+    WajackoyahGeorge: 447,
+    WaihigaDavid: 281
+  },
+  {
+    County: 'BARINGO',
+    RutoWilliam: 175170,
+    RailaOdinga: 41227,
+    WajackoyahGeorge: 474,
+    WaihigaDavid: 220
+  },
+  {
+    County: 'LAIKIPIA',
+    RutoWilliam: 119142,
+    RailaOdinga: 48908,
+    WajackoyahGeorge: 602,
+    WaihigaDavid: 432
+  },
+  {
+    County: 'NAKURU',
+    RutoWilliam: 455864,
+    RailaOdinga: 226052,
+    WajackoyahGeorge: 2435,
+    WaihigaDavid: 1819
+  },
+  {
+    County: 'NAROK',
+    RailaOdinga: 159455,
+    RutoWilliam: 148310,
+    WajackoyahGeorge: 439,
+    WaihigaDavid: 228
+  },
+  {
+    County: 'KAJIADO',
+    RailaOdinga: 158556,
+    RutoWilliam: 148449,
+    WajackoyahGeorge: 933,
+    WaihigaDavid: 686
+  },
+  {
+    County: 'KERICHO',
+    RutoWilliam: 318861,
+    RailaOdinga: 15053,
+    WajackoyahGeorge: 395,
+    WaihigaDavid: 207
+  },
+  {
+    County: 'BOMET',
+    RutoWilliam: 285428,
+    RailaOdinga: 13383,
+    WajackoyahGeorge: 561,
+    WaihigaDavid: 234
+  },
+  {
+    County: 'KAKAMEGA',
+    RailaOdinga: 357857,
+    RutoWilliam: 141166,
+    WajackoyahGeorge: 3459,
+    WaihigaDavid: 1237
+  },
+  {
+    County: 'VIHIGA',
+    RailaOdinga: 114714,
+    RutoWilliam: 67633,
+    WajackoyahGeorge: 1471,
+    WaihigaDavid: 515
+  },
+  {
+    County: 'BUNGOMA',
+    RutoWilliam: 255906,
+    RailaOdinga: 145280,
+    WajackoyahGeorge: 2384,
+    WaihigaDavid: 1581
+  },
+  {
+    County: 'BUSIA',
+    RailaOdinga: 226042,
+    RutoWilliam: 48801,
+    WajackoyahGeorge: 1414,
+    WaihigaDavid: 473
+  },
+  {
+    County: 'SIAYA',
+    RailaOdinga: 371092,
+    RutoWilliam: 4320,
+    WajackoyahGeorge: 734,
+    WaihigaDavid: 208
+  },
+  {
+    County: 'KISUMU',
+    RailaOdinga: 419997,
+    RutoWilliam: 10011,
+    WajackoyahGeorge: 744,
+    WaihigaDavid: 253
+  },
+  {
+    County: 'HOMA BAY',
+    RailaOdinga: 399784,
+    RutoWilliam: 3497,
+    WajackoyahGeorge: 709,
+    WaihigaDavid: 122
+  },
+  {
+    County: 'MIGORI',
+    RailaOdinga: 294136,
+    RutoWilliam: 52525,
+    WajackoyahGeorge: 815,
+    WaihigaDavid: 297
+  },
+  {
+    County: 'KISII',
+    RailaOdinga: 265078,
+    RutoWilliam: 135326,
+    WajackoyahGeorge: 1678,
+    WaihigaDavid: 754
+  },
+  {
+    County: 'NYAMIRA',
+    RailaOdinga: 129025,
+    RutoWilliam: 78356,
+    WajackoyahGeorge: 925,
+    WaihigaDavid: 345
+  },
+  {
+    County: 'NAIROBI CITY',
+    RailaOdinga: 767395,
+    RutoWilliam: 561775,
+    WajackoyahGeorge: 5807,
+    WaihigaDavid: 4390
+  },
+  {
+    County: 'DIASPORA',
+    RailaOdinga: 3727,
+    RutoWilliam: 2190,
+    WajackoyahGeorge: 40,
+    WaihigaDavid: 31
+  }
+];
+
+export default results2022;
