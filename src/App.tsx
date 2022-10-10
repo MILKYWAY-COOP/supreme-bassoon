af8c551c7f07950f4e5354658233e3c4d44d1414
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-10 19:19:19 +0300

    Elections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { Navbar } from './Components';
 import { Home } from './Home/Home';
 
-
 function App() {
   return (
     <div className='App'>
-        <Navbar />
-        <Home />
+      <Navbar />
+      <Home />
+      <div className='footer'>
+        <p>
+          &copy;2022. A{' '}
+          <a
+            href='https://milkyway-coop.github.io/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            MILKYWAY
+          </a>{' '}
+          PRODUCTION. All rights reserved
+        </p>
+      </div>
     </div>
   );
 }
