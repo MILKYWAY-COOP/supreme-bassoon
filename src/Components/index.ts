418ee07287c68ad70e0c8b9e2246a460a512b726
Stephen Muchendu <muchendumaina66@gmail.com>
2022-10-10 19:19:29 +0300

    Elections

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,3 +2,5 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Choice } from './Choice/Choice';
 export { default as Rbar } from './RBar/Rbar';
 export { default as Kenya } from './Kenya/Kenya';
+export { default as Card } from './Card/Card';
+export { default as CountyR } from './CountyR/CountyR';
